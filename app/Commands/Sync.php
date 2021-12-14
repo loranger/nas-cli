850d15a2ecade7946f67bd9ef2c7d0bc11873f2f
Laurent Goussard <loranger@free.fr>
2021-12-14 10:41:09 +0100

    :lipstick: Nicer and reliable progress bar

--- app/Commands/Sync.php
+++ app/Commands/Sync.php
@@ -41,7 +41,7 @@ class Sync extends Command
         $sync = app('conf')->get($label);
 
         $command = Terminal::timeout(0)->command($this->getCommand($sync, true));
-
+// dd($this->getCommand($sync, true));
         $result = $this->task("Analyse des fichiers à synchroniser", function () use ($command, &$response) {
             $response = $command->run();
             return $response->successful();
@@ -70,12 +70,18 @@ class Sync extends Command
         });
 
         if ($this->confirm('Tout synchroniser ?')) {
-            $bar = $this->output->createProgressBar($lines->count());
-            $bar->setFormat(' [%bar%] %percent:3s%%');
+            $bar = $this->output->createProgressBar(100);
+            $bar->setFormat(' [%bar%] <info>%percent:3s%%</info> <fg=blue>%message%</>');
+            $bar->setMessage('--');
+
             $bar->start();
             $command = Terminal::timeout(0)->command($this->getCommand($sync));
             $response = $command->run(function ($type, $buffer) use ($bar) {
-                $bar->advance();
+                if (preg_match('/(\d+)%\s+([a-zA-Z0-9.]+\/s)/mU', $buffer, $matches)) {
+                    $bar->setMessage($matches[2]);
+                    $bar->setProgress($matches[1]);
+                }
+                // $bar->advance();
             });
             $bar->finish();
         }
@@ -89,7 +95,7 @@ class Sync extends Command
 
         return sprintf(
             'rsync -ruv %s --iconv=utf-8-mac,utf-8 %s %s %s',
-            $dry ? '--dry-run' : '',
+            $dry ? '--dry-run' : '--no-inc-recursive --info=progress2',
             $sync->from,
             $sync->to,
             $excludes->implode(' ')
